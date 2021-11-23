c2a052ead452e41f7bda9625d31a8bf6fdd05325
Guillermo Diaz <diazgdev@gmail.com>
2021-11-23 12:37:27 -0600

    Clase 1

---
+++ clase-1/main.js
@@ -0,0 +1,7 @@
+let nombre = prompt("Escribe tu nombre");
+let apellido = prompt("Escribe tu apellido");
+let edad = prompt("Escribe tu edad");
+
+alert("Verifica la consola :)")
+
+console.log("Tu nombre es " + nombre + " " + apellido + " y tienes " + edad + " años.");
